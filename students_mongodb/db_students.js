b51f82eee9833971bee813dc5fcc86dc98ac510a
gabriellejousse <gabrielle.jousse@gmail.com>
2020-11-09 16:53:31 +0100

    jour4 lvl 2

---
+++ students_mongodb/db_students.js
@@ -0,0 +1,80 @@
+/*utiliser db_students*/
+use db_students
+
+/*créer collection(table) et insérer des données */
+db.students.insert({id:1, name:'veronique', city:'paris'})
+db.students.insert({id:2, name:'steeven', city:'lyon'})
+db.students.insert({id:3, name:'marc', city:'marseille'})
+db.students.insert({id:4, name:'nour', city:'lyon'})
+db.students.insert({id:5, name:'romain', city:'paris'})
+db.students.insert({id:6, name:'sophie', city:'paris'})
+
+db.languages.insert({id:1, name:'french'})
+db.languages.insert({id:2,name:'english'})
+db.languages.insert({id:3,name:'german'})
+db.languages.insert ({id:4,name:'spanish'})
+db.languages.insert({id:5, name:'mandarin'})
+
+db.favorites.insert({id:1,class:'maths',sport:'criket',studentsID:2})
+db.favorites.insert({id:2,class:'music',sport:'hit-hop',studentsID:6})
+db.favorites.insert({id:3,class:'arts',sport:'boxing',studentsID:1})
+db.favorites.insert({id:4,class:'litterature',sport:'tennis',studentsID:3})
+db.favorites.insert({id:5,class:'computer sciences',sport:'tennis',studentsID:5})
+db.favorites.insert({id:6,class:'arts',sport:'baseball',studentsID:4})
+
+db.students_languages.insert({id:1,studentsID:1,languageID:1})
+db.students_languages.insert({id:2,studentsID:1,languageID:2})
+db.students_languages.insert({id:3,studentsID:2,languageID:1})
+db.students_languages.insert({id:4,studentsID:2,languageID:3})
+db.students_languages.insert({id:5,studentsID:3,languageID:1})
+db.students_languages.insert({id:6,studentsID:4,languageID:1})
+db.students_languages.insert({id:7,studentsID:4,languageID:2})
+db.students_languages.insert({id:8,studentsID:4,languageID:4})
+db.students_languages.insert({id:9,studentsID:4,languageID:5})
+db.students_languages.insert({id:10,studentsID:5,languageID:1})
+db.students_languages.insert({id:11,studentsID:5,languageID:5})
+db.students_languages.insert({id:12,studentsID:6,languageID:1})
+db.students_languages.insert({id:13,studentsID:6,languageID:2})
+db.students_languages.insert({id:14,studentsID:6,languageID:3})
+
+/* requêtes lvl 1 */
+db.students.find({id:3})
+
+db.students.find({id:6})
+
+/* la premiere accolade dans le find est l'équivalent du WHERE (condition) */
+db.students.find({id:1}, {name:1, city:1, _id:0})
+
+db.students.find({id:2}, {name:1, _id:0})
+
+db.students.find({city:"paris"})
+
+db.students.find({city:"lyon"}, {name:1, _id:0})
+
+/* requêtes lvl 2 */
+
+db.students.aggregate([
+{
+   $lookup:
+     {
+       from: "favorites",
+       localField: "id",
+       foreignField: "student_id",
+       as: "new"
+     },
+     {   $unwind:"$new" },
+
+       // define some conditions here 
+       {
+           $match:{
+               $and:[{"id" : "student_id"}]
+           }
+       },
+
+       // define which fields are you want to fetch
+       {   
+           $project:{
+               "id":5
+           } 
+       }
+}])
